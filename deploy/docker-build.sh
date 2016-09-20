8a1e276a6f89cfad64c8d967d0fe1fe6703fd503
Marc Epard <mepard@me.com>
2016-09-20 10:35:33 -0500

    Prevent build.py from changing branches on qtbase and qtwebkit.

--- deploy/docker-build.sh
+++ deploy/docker-build.sh
@@ -48,7 +48,8 @@ cd $BUILD_PATH && cp -rp $SOURCE_PATH . && cd src
 echo
 
 echo "Compiling PhantomJS..." && sleep 1
-python build.py --confirm --release --qt-config="-no-pkg-config" --git-clean-qtbase --git-clean-qtwebkit
+#python build.py --confirm --release --qt-config="-no-pkg-config" --git-clean-qtbase --git-clean-qtwebkit
+python build.py --confirm --release --qt-config="-no-pkg-config" --skip-git 
 echo
 
 echo "Stripping the executable..." && sleep 1
